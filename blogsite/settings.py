e6a1170e95ad36b9ac174ad0e31433b10b6a8dd6
hiscloan <thorin.pickard@gmail.com>
2025-06-22 22:35:17 +0200

    Messing with db stuff

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'blogsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
