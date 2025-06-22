43a06ede4185c0483b4b8831d41d4d47e9f44f2f
hiscloan <thorin.pickard@gmail.com>
2025-06-22 22:51:07 +0200

    Changed static settings.py

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files (User-uploaded content)
 MEDIA_URL = '/media/'
