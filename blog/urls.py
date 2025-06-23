57de44f3c11507bba70b746cadf053b3be9a0ffd
hiscloan <thorin.pickard@gmail.com>
2025-06-23 21:14:43 +0200

    Fixed logout to now use POST method and hid the button under a drop down

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,8 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
-
+from django.contrib.auth import logout
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
@@ -13,4 +14,6 @@ urlpatterns = [
     path('about/', AboutView.as_view(), name='blog-about'),
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logged_out.html'), name='logout')
+  
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
