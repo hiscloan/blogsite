b3b906c212daa2c5ddbec06c0ae8fbc2fd0acbde
hiscloan <thorin.pickard@gmail.com>
2025-06-23 20:12:25 +0200

    added +static under url paths

--- blog/urls.py
+++ blog/urls.py
@@ -13,5 +13,4 @@ urlpatterns = [
     path('about/', AboutView.as_view(), name='blog-about'),
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
